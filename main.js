7e8a19a0034868f253e2ba52bfbc7ca2e2882de2
Till M. Kammertöns <till@kammertoens.de>
2023-10-25 19:49:39 +0300

    initial

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-import * as THREE from 'three';
-import { MTLLoader } from 'three/addons/loaders/MTLLoader.js';
-import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+// import * as THREE from 'three';
+// import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+import * as THREE from 'https://unpkg.com/three@0.156.1/build/three.module.js';
+import { OrbitControls } from 'https://unpkg.com/three@0.156.1/examples/jsm/controls/OrbitControls.js';
 
 let mouseX = 0;
 let mouseY = 0;
@@ -25,27 +26,37 @@ function init() {
 	scene.add( ambientLight );
 	
 	const camlight = new THREE.PointLight( 0xffffff, 10 );
+	camlight.position.x = 1;
+	camlight.position.y = 1;
 	camera.add( camlight );
-
+	
+	// const spot = new THREE.PointLight( 0xffffff, 10 );
+	// spot.position.z = 2.5;
+	// spot.position.x = 1;
+	// spot.position.y = 1;
+	// scene.add(spot);
+	
 	particleLight = new THREE.Mesh(
 		new THREE.SphereGeometry( 0, 8, 8 ),
 		new THREE.MeshBasicMaterial( { color: 0xffffff } )
 	);
 	scene.add( particleLight );
-	particleLight.add( new THREE.PointLight( 0xffffff, 0 ) );
+	particleLight.add( new THREE.PointLight( 0xffffff, 10 ) );
 	
 	scene.add( camera );
 	
 
 	// model
+	var ratio;
 	sitesTexture = new THREE.TextureLoader().load('./sites.png');
-	sitesTexture.repeat.set(4,3)
+	sitesTexture.repeat.set(2, 1.2);
 	sitesTexture.wrapS = THREE.RepeatWrapping;
 	sitesTexture.wrapT = THREE.RepeatWrapping;
 	const geometry = new THREE.SphereGeometry(0.9, 128, 64);
 	const material = new THREE.MeshStandardMaterial({
 		map: sitesTexture,
 		transparent: true,
+		side: THREE.DoubleSide,
 	});
 	sphere = new THREE.Mesh(geometry, material);
 	sphere.position.x = 0;
@@ -57,7 +68,7 @@ function init() {
 	spaceTexture = new THREE.TextureLoader().load('./bg.jpg');
 	spaceTexture.wrapS = THREE.RepeatWrapping;
 	spaceTexture.wrapT = THREE.RepeatWrapping;
-	spaceTexture.repeat.set(2, 2);
+	spaceTexture.repeat.set(3, 3);
 
 	scene.background = spaceTexture;
 	scene.backgroundIntensity = 0.05;
@@ -69,7 +80,7 @@ function init() {
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	document.body.appendChild( renderer.domElement );
 
-	controls = new  OrbitControls( camera, renderer.domElement );
+	controls = new OrbitControls( camera, renderer.domElement );
 	camera.position.set( -0.8, 0.5, -2.2 );
 	controls.enabled = false;
 	controls.update();
